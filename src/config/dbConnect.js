0c7f74f45e26ba95421990c638f026308afe52a9
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2024-11-11 12:37:10 -0300

    "Valida~oes"

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://alura:123@alura.dkjed.mongodb.net/alura-node");
+mongoose.connect(process.env.STRING_CONEXAO_DB);
+
 
 let db = mongoose.connection;
 
 export default db;
+
+//zfheAvHXFlyOZDAO
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,10 +3,30 @@ import mongoose from "mongoose";
 const livroSchema = new mongoose.Schema(
   {
     id: {type: String},
-    titulo: {type: String, required: true},
-    autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true},
-    editora: {type: String, required: true},
-    numeroPaginas: {type: Number}
+    titulo: {
+      type: String,
+      required: [true, "O título do livro é obrigatorio"]
+    },
+    autor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'autores',
+       required: [true, "O(a) autor(a) é obrigatório"]
+      },
+    editora: {
+      type: String,
+      required: [true, " Campo a editora é obrigatório"],
+      enum:{
+        values:["Casa do código", "Alura"],
+        message: "A editora {VALUE} informada não é um valor permitido"
+      }
+    },
+    numeroPaginas:
+     {
+      type: Number,
+      min: [5,"Valor informado:{VALUE} - O numero de paginas deve estar entre 5 e 5000"],
+      max: [5000,"Valor informado:{VALUE} - O numero de paginas deve estar entre 5 e 5000"]
+    }
+     
   }
 );
 
