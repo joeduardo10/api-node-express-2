c1acce83cac7bf0ccac9ce7bef29cc532a32b2b3
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2024-11-13 22:30:05 -0300

    "fim"

--- aulas/api-node-express-2-aula-5/src/controllers/autoresController.js
+++ aulas/api-node-express-2-aula-5/src/controllers/autoresController.js
@@ -2,17 +2,17 @@ import NaoEncontrado from "../erros/NaoEncontrado.js";
 import { autores } from "../models/index.js";
 
 class AutorController {
-
   static listarAutores = async (req, res, next) => {
     try {
-      const autorResultado = autores.find();
-      req.resultado = autorResultado;
+      const autoresResultado = autores.find();
+
+      req.resultado = autoresResultado;
 
       next();
     } catch (erro) {
-      next(erro);
+      res.status(500).json({ message: "Erro interno no servidor" });
     }
-  }
+  };
 
   static listarAutorPorId = async (req, res, next) => {
     try {
@@ -25,34 +25,35 @@ class AutorController {
       } else {
         next(new NaoEncontrado("Id do Autor não localizado."));
       }
-
     } catch (erro) {
       next(erro);
     }
-  }
+  };
 
   static cadastrarAutor = async (req, res, next) => {
     try {
       let autor = new autores(req.body);
+
       const autorResultado = await autor.save();
 
       res.status(201).send(autorResultado.toJSON());
     } catch (erro) {
       next(erro);
     }
-  }
-
+  };
 
   static atualizarAutor = async (req, res, next) => {
     try {
       const id = req.params.id;
-      const autorResultado = await autores.findByIdAndUpdate(id, { $set: req.body });
-      if(autorResultado !== null){
+  
+      const autorResultado = await autores.findByIdAndUpdate(id, {$set: req.body});
+
+      if (autorResultado !== null) {
         res.status(200).send({message: "Autor atualizado com sucesso"});
-      }else{
-        next(new NaoEncontrado("is do autor não localizado"))
+      } else {
+        next(new NaoEncontrado("Id do Autor não localizado."));
       }
-      
+
     } catch (erro) {
       next(erro);
     }
@@ -63,16 +64,17 @@ class AutorController {
       const id = req.params.id;
 
       const autorResultado = await autores.findByIdAndDelete(id);
-      if(autorResultado !== null){
 
-        res.status(200).send({ message: "Autor removido com sucesso" });
-      }else{
-        next(NaoEncontrado("Id do autor não Localizado"))
+
+      if (autorResultado !== null) {
+        res.status(200).send({message: "Autor removido com sucesso"});
+      } else {
+        next(new NaoEncontrado("Id do Autor não localizado."));
       }
     } catch (erro) {
       next(erro);
     }
-  }
+  };
 }
 
-export default AutorController
+export default AutorController;
--- aulas/api-node-express-2-aula-5/src/routes/livrosRoutes.js
+++ aulas/api-node-express-2-aula-5/src/routes/livrosRoutes.js
@@ -10,6 +10,6 @@ router
   .get("/livros/:id", LivroController.listarLivroPorId)
   .post("/livros", LivroController.cadastrarLivro)
   .put("/livros/:id", LivroController.atualizarLivro)
-  .delete("/livros/:id", LivroController.excluirLivro)
+  .delete("/livros/:id", LivroController.excluirLivro);
 
 export default router;   
